a892d08efc79394dc6e8ea1c1e950f8fa4b4c8de
sailaxmi <sailaxmikomirineni@gmail.com>
2023-07-13 14:00:41 +0000

    solution

--- src/components/GoogleSuggestions/index.js
+++ src/components/GoogleSuggestions/index.js
@@ -1,34 +1,66 @@
 // Write your code here
 import './index.css'
-import {Component} from 'react' 
-import SuggestionItem from "../SuggestionItem"
+import {Component} from 'react'
+import SuggestionItem from '../SuggestionItem'
 
-class GoogleSuggestions extends Component{
-    state = {newSuggestionsList:suggestionsList}
-    render() {
-        const {newSuggestionsList}=this.state
-        
-        return(
-            <div className="bg-container">
-                <img src="https://assets.ccbp.in/frontend/react-js/google-logo.png" 
-                alt="google-logo" className="google-img"/>
-                <div className="google-search-container">
-                     <div className="search-container">
-                         <img src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
-                         alt="search-icon" className="search-icon" /> 
-                         <input type="search" placeholder="Search Google"/>
-                     </div>
-                     <ul className="suggestion-container">
-                         {
-                           newSuggestionsList.map((each) => (
-                            each.suggestion
-                           ))
-                         }
-                     </ul>
-                </div>
-            </div>
-        )
-    }
+class GoogleSuggestions extends Component {
+  state = {searchInput: ''}
+
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  onChangevalue = suggestion => {
+    this.setState({searchInput: suggestion})
+  }
+
+  render() {
+    const {searchInput} = this.state
+    const {suggestionsList} = this.props
+    const newSuggestionsList = suggestionsList
+
+    const searchResult = newSuggestionsList.filter(each =>
+      each.suggestion.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+
+    return (
+      <div className="bg-container">
+        <div>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
+            alt="google-logo"
+            className="google-img"
+          />
+        </div>
+        <div className="google-search-container">
+          <div className="search-container">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
+              alt="search-icon"
+              className="search-icon"
+            />
+            <input
+              type="search"
+              value={searchInput}
+              placeholder="Search Google"
+              className="search"
+              onChange={this.onChangeSearchInput}
+            />
+          </div>
+
+          <ul className="suggestion-container">
+            {searchResult.map(each => (
+              <SuggestionItem
+                details={each}
+                key={each.id}
+                onChangevalue={this.onChangevalue}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default GoogleSuggestions
--- src/components/SuggestionItem/index.js
+++ src/components/SuggestionItem/index.js
@@ -1,17 +1,26 @@
 // Write your code here
-import './index.css' 
+import './index.css'
 
-const SuggestionItem = (props)=> {
+const SuggestionItem = props => {
+  const {details, onChangevalue} = props
+  const {suggestion} = details
 
-    return (
-       <li className="suggestion-container">
-           <div className="suggestion-item">
-               <p className="para"> {} </p> 
-               <img src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
-                    alt="arrow-icon" className="arrow"/>
-           </div>
-       </li>
-    )
+  const onChangesearch = () => {
+    onChangevalue(suggestion)
+  }
+
+  return (
+    <li className="suggestion-item-container">
+      <p className="para"> {suggestion} </p>
+      <button type="button" className="button" onClick={onChangesearch}>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+          alt="arrow"
+          className="arrow"
+        />
+      </button>
+    </li>
+  )
 }
 
 export default SuggestionItem
